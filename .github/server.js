8341b49fcad4392f6003b445fdcc7e001eab87c2
Andre Freeman <asf@mail.usf.edu>
2020-06-03 20:41:52 -0400

    Made Server Post

--- .github/server.js
+++ .github/server.js
@@ -5,6 +5,19 @@ const server = express()
 
 server.use(express.json())
 
+server.post("/users", (req, res) => {
+    if (!req.body.name) {
+        return res.status(400).json({
+            message: "Need a name for the User",
+        })
+    }
+    const newUser = db.createUser({
+        name: req.body.name,
+    })
+    // 201 User Create
+    res.status(201).json(newUser)
+})
+
 server.listen(8080, ()=> {
     console.log("server started on port 8080")
 })
