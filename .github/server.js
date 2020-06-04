c59db1021f79aec1406c4a6581c885ed20a0d625
Andre Freeman <asf@mail.usf.edu>
2020-06-03 20:50:55 -0400

    Created Put, Get, and delete endpoints

--- .github/server.js
+++ .github/server.js
@@ -14,8 +14,50 @@ server.post("/users", (req, res) => {
     const newUser = db.createUser({
         name: req.body.name,
     })
-    // 201 User Create
     res.status(201).json(newUser)
+})
+
+server.get("/users", (req, res) => {
+    const users = db.getUsers()
+    res.json(users)
+})
+
+server.get("/users/:id", (req, res) => {
+    const user = db.getUserById(req.params.id)
+
+    if(user) {
+        res.json(user)
+    } else {
+        res.status(404).json({
+            message: "User not found",
+        })
+    }
+
+})
+
+server.put("/users/:id", (req, res) => {
+    const user = db.getUserById(req.params.id)
+
+    if (user) {
+        const updatedUser = db.updateUser(user.id, {
+            name: req.body.name || user.name,
+        })
+        res.json(updatedUser)
+    } else {
+        return res.status(400).json({
+            message: "Need a name for the User",
+    })}
+
+})
+
+server.delete("/users/:id", (req, res) => {
+    const user = db.getUserById(req.params.id)
+
+    if(user) {
+        deleteUser = db.deleteUser(user.id)
+} else { return res.status(404).json({
+    message: "User Not Found",
+
 })
 
 server.listen(8080, ()=> {
