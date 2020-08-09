71199ea7239429f4b6142570dc5d722338aa1501
Andre Freeman <asf@mail.usf.edu>
2020-08-09 19:03:37 -0400

    Cleaned up code a bit

--- .github/server.js
+++ .github/server.js
@@ -1,72 +1,81 @@
-const express = require("express")
-const db = require("./data")
+const express = require("express");
+const db = require("./data");
 
-const server = express()
+const server = express();
 
-server.use(express.json())
+server.use(express.json());
+
+//Get All Users
 
 server.get("/users", (req, res) => {
-    const users = db.getUsers()
-    res.json(users)
-})
+  const users = db.getUsers();
+  res.json(users);
+});
+
+//Get User By ID
 
 server.get("/users/:id", (req, res) => {
-    const user = db.getUserById(req.params.id)
+  const user = db.getUserById(req.params.id);
 
-    if(user) {
-        res.json(user)
-    } else {
-        res.status(404).json({
-            message: "User not found",
-        })
-    }
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).json({
+      message: "User not found"
+    });
+  }
+});
 
-})
+// Add New User
 
 server.post("/users", (req, res) => {
-    if (!req.body.name) {
-        return res.status(400).json({
-            message: "User needs a Name",
-        })
-    }
-    const newUser = db.createUser({
-        name: req.body.name,
-        bio: req.body.bio,
-    })
-    res.status(201).json(newUser)
-})
+  if (!req.body.name) {
+    return res.status(400).json({
+      message: "User needs a Name"
+    });
+  }
+  const newUser = db.createUser({
+    name: req.body.name,
+    bio: req.body.bio
+  });
+  res.status(201).json(newUser);
+});
+
+//Update User
 
 server.put("/users/:id", (req, res) => {
-    const user = db.getUserById(req.params.id)
+  const user = db.getUserById(req.params.id);
 
-    if (user) {
-        const updatedUser = db.updateUser(user.id, {
-            name: req.body.name || user.name,
-            bio: req.body.bio || user.bio,
-        })
-        res.json(updatedUser)
-    } else {
-        return res.status(400).json({
-            message: "Need a name for the User",
-    })}
+  if (user) {
+    const updatedUser = db.updateUser(user.id, {
+      name: req.body.name || user.name,
+      bio: req.body.bio || user.bio
+    });
+    res.json(updatedUser);
+  } else {
+    return res.status(400).json({
+      message: "Need a name for the User"
+    });
+  }
+});
 
-})
+// Delete User
 
 server.delete("/users/:id", (req, res) => {
-    const user = db.getUserById(req.params.id)
-    if (user) {
-        db.deleteUser(req.params.id)
+  const user = db.getUserById(req.params.id);
+  if (user) {
+    db.deleteUser(req.params.id);
 
-        res.status(200).json({
-            message: "User Erased"
-        })
-    } else {
-        res.status(404).json({
-            message: "User not found"
-        })
-    }
-})
+    res.status(200).json({
+      message: "User Erased"
+    });
+  } else {
+    res.status(404).json({
+      message: "User not found"
+    });
+  }
+});
 
-server.listen(8080, ()=> {
-    console.log("server started on port 8080")
-})
+server.listen(8080, () => {
+  console.log("server started on port 8080");
+});
