311807c5be80f53ae311549ca2826f014f638e8c
Andre Freeman <asf@mail.usf.edu>
2020-08-09 18:50:29 -0400

    Created 2 GET endpoints

--- .github/server.js
+++ .github/server.js
@@ -5,6 +5,24 @@ const server = express()
 
 server.use(express.json())
 
+server.get("/users", (req, res) => {
+    const users = db.getUsers()
+    res.json(users)
+})
+
+server.get("/users/:id", (req, res) => {
+    const user = db.getUserById(req.params.id)
+
+    if(user) {
+        res.json(user)
+    } else {
+        res.status(404).json({
+            message: "User not found",
+        })
+    }
+
+})
+
 server.post("/users", (req, res) => {
     if (!req.body.name) {
         return res.status(400).json({
